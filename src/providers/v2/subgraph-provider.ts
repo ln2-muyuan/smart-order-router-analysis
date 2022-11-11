f751b5ac42ab3ee007e7d63b750bdc7ca728828c
Mu'Ye <lmy2019@outlook.com>
2022-11-11 14:05:23 +0800

    update

--- src/providers/v2/subgraph-provider.ts
+++ src/providers/v2/subgraph-provider.ts
@@ -1,4 +1,4 @@
-
+import { gql, GraphQLClient } from 'graphql-request';
 import { Token } from '@uniswap/sdk-core'
 
 
@@ -10,7 +10,25 @@ export interface IV2SubgraphProvider {
 }
 
 export class V2SubgraphProvider implements IV2SubgraphProvider {
+    private client: GraphQLClient;
+
+    constructor() {
+        this.client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2');
+    }
+
+
     public async getPools(tokenIn: Token, tokenOut: Token): Promise<any> {
+
+        const query = gql`
+    {
+        bundle(id: "1" ) {
+            ethPrice
+        }
+    }`
+    ;
+        
+        
+        
         return null
     }
 }
